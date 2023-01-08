65819e1a9f281f9ae68a993408b4c991eef9efd3
Gustavo Santos <gustavinhodossantos123@gmail.com>
2023-01-08 19:26:42 -0300

    feat: Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,26 @@ export default function App({ Component, pageProps }: AppProps) {
           name="description"
           content="Portifólio para ficar registrado projetos feitos por mim"
         />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
       </Head>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyles />
